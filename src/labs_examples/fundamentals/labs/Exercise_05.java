e3082a6085ff0632527cfad410111b3f18c77f58
Antoine Lovelace <lovelace.a.1@gmail.com>
2020-11-28 21:22:50 -0500

    completed Exercise_05

--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -15,13 +15,23 @@ public class Exercise_05 {
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
 
+        int x = str.length();
+        System.out.println("The legnth of the string str is " + x);
+
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
 
-        // please concatenate str & str2 and set the result to a new String variable below
+        boolean equalString = str.equalsIgnoreCase(str2);
+        System.out.println("Are str & str2 the same? " + equalString);
 
+        // please concatenate str & str2 and set the result to a new String variable below
+        
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+
+        String upstr = str.toUpperCase();
+        System.out.println("str in all caps is " + upstr);
+        System.out.println("My first program is called: " + str2.replace('h','H') + " World!");
 
     }
 
