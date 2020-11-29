4525712e2694f37763aa2e4507f13248266ad0eb
Antoine Lovelace <lovelace.a.1@gmail.com>
2020-11-29 17:27:54 -0500

    Fundamentals Exercise_05 is completed along with the first three exercises of Java operators.

--- src/labs_examples/datatypes_operators/labs/Exercise_01.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_01.java
@@ -17,6 +17,14 @@ public class Exercise_01 {
     public static void main(String[] args) {
 
         // write code here
+        int a = 3;
+        double b = 3.14278493;
+        float c = 1.2748535f;
+        char d = 'A';
+        System.out.println("a is " + a);
+        System.out.println("b is " + b);
+        System.out.println("c is " + c);
+        System.out.println("d is " + d);
 
     }
 }
--- src/labs_examples/datatypes_operators/labs/Exercise_03.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_03.java
@@ -12,6 +12,17 @@ class ArithmeticOperators {
 
         // write your code below
 
+        int a = 3;
+        int b = 4;
+        int c = a + b;
+        int d = c - b;
+        int e = c * d;
+        int f = e / a;
+        int g = 10 % 4;
+        System.out.println(a + " + " + b + " = " + c);
+        System.out.println(f + " which is f, is also equal to " + c + ". which is also c");
+        System.out.println("ten divided by four has a remainder of " + g);
+
     }
 
 }
