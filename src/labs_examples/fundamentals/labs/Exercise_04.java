a4c8b84f610ba8afa138ffea3f3d39a2460a9194
Antoine Lovelace <aj@Antoines-iMac.local>
2020-11-28 20:12:52 -0500

    Exercise_04 is completed

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -13,9 +13,43 @@ public class Exercise_04 {
     public static void main(String[] args) {
 
         int i = 1;
+
         System.out.println("int i is: " + i);
 
         // write your code below
+
+        byte a = 3;
+
+        System.out.println("a = " + a);
+
+        short b = -50;
+
+        System.out.println("b = " + b);
+
+        int c = 2000;
+
+        System.out.println("c = " + c);
+
+        long d = 1000000000l;
+
+        System.out.println("d = " + d);
+
+        float e = 1.567890f;
+
+        System.out.println("e = " + e);
+
+        double f = 3.1427986467824;
+
+        System.out.println("f = " + f);
+
+        boolean success = true;
+
+        System.out.println("This code is done correctly = " + success);
+
+        char z = 'g';
+
+        System.out.println("The letter sequence ends at " + z);
+
 
     }
 
