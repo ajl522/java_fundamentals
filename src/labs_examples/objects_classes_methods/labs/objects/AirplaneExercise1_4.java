6cbacfc169a6ddb7213ecd173643351758630e6a
Antoine Lovelace <antoinelovelace@Antoines-MacBook-Air.local>
2020-12-16 13:10:14 -0500

    updated exercise 4 in objects

--- src/labs_examples/objects_classes_methods/labs/objects/AirplaneExercise1_4.java
+++ src/labs_examples/objects_classes_methods/labs/objects/AirplaneExercise1_4.java
@@ -3,7 +3,7 @@ package labs_examples.objects_classes_methods.labs.objects;
 
 import sun.security.krb5.internal.crypto.Des;
 
-public class AirplaneExercise{
+public class AirplaneExercise1_4{
     public static void main(String[] args) {
         Passengers a = new Passengers(120);
         Eta b = new Eta(1800);
@@ -12,6 +12,31 @@ public class AirplaneExercise{
         Airplane e = new Airplane(a, b, c, d, 160.5, 122.3);
 
         System.out.println(e.toString());
+
+        Passengers a2 = new Passengers(160);
+        Eta b2 = new Eta(750);
+        Destination c2 = new Destination("Los Angeles, CA");
+        Departure d2 = new Departure("Atlanta, GA");
+
+        Airplane g = new Airplane(a2, b2, c2,d2, 175.2, 130.5);
+
+        Passengers a3 = new Passengers(80);
+        Eta b3 = new Eta(1120);
+        Destination c3 = new Destination("New York, NY");
+        Departure d3 = new Departure("Atlanta, GA");
+
+        Airplane f = new Airplane(a3,b3, c3, d3, 125.7, 60.3 );
+
+        Passengers a4 = new Passengers(110);
+        Eta b4 = new Eta(1045);
+        Destination c4 = new Destination("Denver, CO");
+        Departure d4 = new Departure("Atlanta, GA");
+
+        Airplane i = new Airplane(a4, b4, c4, d4, 115.8, 95.6);
+
+        System.out.println(g.toString());
+        System.out.println(f.toString());
+        System.out.println(i.toString());
 
     }
 
